55b470193235fc0f1b7797762e14773d4e36397a
Sam Hakimane <hsamhakimane@gmail.com>
2020-02-27 11:40:46 +0100

    Initalized project

---
+++ src/main/java/bfst20/Map/Launcher.java
@@ -0,0 +1,15 @@
+package bfst20.Map;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       System.out.println("HEY");
+    }
+
+	public static void main(String[] args) {
+        launch(args);
+	}
+}
