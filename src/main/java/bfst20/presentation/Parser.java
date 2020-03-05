9e70a30e091e27186600d2ab68ccfcb20d3248ae
Sam Hakimane <hsamhakimane@gmail.com>
2020-03-05 10:10:13 +0100

    XML parser initialization

--- src/main/java/bfst20/presentation/Parser.java
+++ src/main/java/bfst20/presentation/Parser.java
@@ -1,9 +1,34 @@
 package bfst20.presentation;
 
-public class Parser{
-    private Parser(){}
+import java.io.Reader;
+import java.io.StringReader;
 
-    public static void parseString(){
-        //TODO: Write code.
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import static javax.xml.stream.XMLStreamConstants.*;
+
+public class Parser {
+    private Parser() {
+    }
+
+    public static void parseString(String string) throws XMLStreamException {
+        Reader stringReader = new StringReader(string);
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLStreamReader reader = factory.createXMLStreamReader(stringReader);
+
+        while(reader.hasNext()){
+            reader.next();
+
+            switch(reader.getEventType()){
+                case START_ELEMENT:
+                    
+                    break;
+                case END_ELEMENT:
+                    
+                    break;
+            }
+        }
     }
 }
