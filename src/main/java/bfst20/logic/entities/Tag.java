d266ca79f3ce841c464f637bb3340e123bc0b452
Sam Hakimane <samh@itu.dk>
2020-03-11 15:31:38 +0100

    Implemented drawing of nodes

--- src/main/java/bfst20/logic/entities/Tag.java
+++ src/main/java/bfst20/logic/entities/Tag.java
@@ -1,20 +1,24 @@
 package bfst20.logic.entities;
 
 import bfst20.logic.interfaces.OSMElement;
+import javafx.scene.canvas.GraphicsContext;
 
 import javax.xml.stream.XMLStreamReader;
 
-public class Tag implements OSMElement{
+public class Tag implements OSMElement {
     private String key, value;
     private XMLStreamReader reader;
 
-    public Tag(){}
+    public Tag() {
+    }
 
-    public String getKey(){
+    public String getKey() {
         return key;
     }
 
-    public String getValue(){ return value; }
+    public String getValue() {
+        return value;
+    }
 
     @Override
     public void setValues() {
@@ -22,10 +26,6 @@ public class Tag implements OSMElement{
         value = String.valueOf(reader.getAttributeValue(null, "v"));
     }
 
-    @Override
-    public void addSubElements(OSMElement subElement) {
-
-    }
 
     @Override
     public void setReader(XMLStreamReader reader) {
--- src/main/java/bfst20/logic/entities/Way.java
+++ src/main/java/bfst20/logic/entities/Way.java
@@ -1,32 +1,31 @@
 package bfst20.logic.entities;
 
+import bfst20.logic.interfaces.Drawable;
 import bfst20.logic.interfaces.OSMElement;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import javax.xml.stream.XMLStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Way implements OSMElement {
+public class Way implements OSMElement, Drawable {
     private long id;
-    private List<OSMElement> subElements;
-    private List<Long> nodeIds;
+    private List<OSMElement> tags;
+    private List<OSMElement> nodes;
     private XMLStreamReader reader;
 
-    public Way(){
-        subElements = new ArrayList<>();
-        nodeIds = new ArrayList<>();
+    public Way() {
+        tags = new ArrayList<OSMElement>();
+        nodes = new ArrayList<OSMElement>();
     }
 
-    public long getId(){
+    public long getId() {
         return id;
     }
 
-    public List<Long> getNodeIds(){
-        return nodeIds;
-    }
-
-    public void addId(){
-        nodeIds.add(Long.parseLong(reader.getAttributeValue(null, "ref")));
+    public void addNode(OSMElement node) {
+        this.nodes.add(node);
     }
 
     @Override
@@ -34,13 +33,34 @@ public class Way implements OSMElement {
         id = Long.parseLong(reader.getAttributeValue(null, "id"));
     }
 
-    @Override
-    public void addSubElements(OSMElement osmElement) {
-        subElements.add(osmElement);
+    public void addTag(OSMElement tag) {
+        this.tags.add(tag);
     }
 
     @Override
     public void setReader(XMLStreamReader reader) {
         this.reader = reader;
+    }
+
+    @Override
+    public void Draw(GraphicsContext gc) {
+        Node start = (Node) nodes.get(0);
+
+        gc.beginPath();
+        gc.moveTo(start.getLatitude(), start.getLongitude());
+
+        for(int i = 0; i< nodes.size(); i++){
+            Node node1 = (Node) nodes.get(i);
+            if(i+1 == nodes.size() - 1) return;
+            Node node2 = (Node) nodes.get(i+1);
+
+            gc.beginPath();
+            gc.moveTo(node1.getLongitude(), node1.getLatitude());
+            gc.lineTo(node2.getLongitude(), node2.getLatitude());
+            gc.stroke();
+        }
+
+        gc.stroke();
+
     }
 }
