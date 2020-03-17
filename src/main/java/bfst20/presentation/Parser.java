1cb71c7a3dee0a175c78ea61d1cd5ccd132bba2f
HeeysamH <samh@itu.dk>
2020-03-17 14:36:52 +0100

    Allowing rendere not to render unwanted ways.

--- src/main/java/bfst20/presentation/Parser.java
+++ src/main/java/bfst20/presentation/Parser.java
@@ -19,7 +19,6 @@ import static javax.xml.stream.XMLStreamConstants.*;
 
 public class Parser {
 
-
     private List<Way> OSMWays;
     private Map<Long, Node> nodeMap;
     private List<Relation> OSMRelations;
@@ -33,26 +32,38 @@ public class Parser {
         OSMRelations = new ArrayList<>();
     }
 
-    public List<Way> getOSMWays (){
+    public List<Way> getOSMWays() {
         return OSMWays;
     }
 
-    public float getMinLat(){return minlat;}
-    public float getMaxLon(){return maxlon;}
-    public float getMaxLat(){return maxlat;}
-    public float getMinLon(){return minlon;}
+    public float getMinLat() {
+        return minlat;
+    }
 
-    public List<Relation> getOSMRelations (){
+    public float getMaxLon() {
+        return maxlon;
+    }
+
+    public float getMaxLat() {
+        return maxlat;
+    }
+
+    public float getMinLon() {
+        return minlon;
+    }
+
+    public List<Relation> getOSMRelations() {
         return OSMRelations;
     }
 
-    public List<Relation> getIslandRelations (){
+    public List<Relation> getIslandRelations() {
         List<Relation> islands = new ArrayList<>();
 
-        for(Relation relation : OSMRelations){
+        for (Relation relation : OSMRelations) {
             String place = relation.getTag("place");
-            if(place == null) continue;
-            if(place.equals("island")){
+            if (place == null)
+                continue;
+            if (place.equals("island")) {
                 islands.add(relation);
             }
         }
@@ -60,8 +71,8 @@ public class Parser {
         return islands;
     }
 
-    public static Parser getInstance(){
-        if(isLoaded == false){
+    public static Parser getInstance() {
+        if (isLoaded == false) {
             isLoaded = true;
             parser = new Parser();
         }
@@ -91,49 +102,49 @@ public class Parser {
 
     private List<Way> parse(XMLStreamReader reader) throws XMLStreamException {
 
+        OSMElement lastElement = null;
+
         while (reader.hasNext()) {
-            OSMElement lastElement = null;
+            reader.next();
 
-            while (reader.hasNext()) {
-                reader.next();
+            switch (reader.getEventType()) {
+                case START_ELEMENT:
+                    String tagName = reader.getLocalName();
 
-                switch (reader.getEventType()) {
-                    case START_ELEMENT:
-                        String tagName = reader.getLocalName();
+                    switch (tagName) {
+                        case "bounds":
+                            minlat = -Float.parseFloat(reader.getAttributeValue(null, "maxlat"));
+                            maxlon = 0.56f * Float.parseFloat(reader.getAttributeValue(null, "maxlon"));
+                            maxlat = -Float.parseFloat(reader.getAttributeValue(null, "minlat"));
+                            minlon = 0.56f * Float.parseFloat(reader.getAttributeValue(null, "minlon"));
+                            break;
+                        case "node":
+                            addNodeToMap(reader);
+                            break;
+                        case "way":
+                            lastElement = addWayToList(reader);
+                            break;
+                        case "nd":
+                            addSubElementToWay(reader);
+                            break;
+                        case "relation":
+                            lastElement = addRelationToList(reader);
+                            break;
+                        case "tag":
+                            if (lastElement instanceof Relation) {
+                                addTagToRelation(reader);
+                            } else if (lastElement instanceof Way) {
+                                addTagToWay(reader);
+                            }
+                            break;
+                        case "member":
+                            addMemberToRelation(reader);
+                            break;
+                    }
+                    break;
+                case END_ELEMENT:
 
-                        switch (tagName) {
-                            case "bounds":
-                                minlat = -Float.parseFloat(reader.getAttributeValue(null, "maxlat"));
-                                maxlon = 0.56f * Float.parseFloat(reader.getAttributeValue(null, "maxlon"));
-                                maxlat = -Float.parseFloat(reader.getAttributeValue(null, "minlat"));
-                                minlon = 0.56f * Float.parseFloat(reader.getAttributeValue(null, "minlon"));
-                                break;
-                            case "node":
-                                addNodeToMap(reader);
-                                break;
-                            case "way":
-                                addWayToList(reader);
-                                break;
-                            case "nd":
-                                addSubElementToWay(reader);
-                                break;
-                            case "relation":
-                                lastElement = addRelationToList(reader);
-                                break;
-                            case "tag":
-                                if (lastElement instanceof Relation) {
-                                     addTagToRelation(reader);
-                                }
-                                break;
-                            case "member":
-                                addMemberToRelation(reader);
-                                break;
-                        }
-                        break;
-                    case END_ELEMENT:
-
-                        break;
-                }
+                    break;
             }
         }
         return OSMWays;
@@ -162,6 +173,14 @@ public class Parser {
 
     }
 
+    private void addTagToWay(XMLStreamReader reader) {
+        Way way = OSMWays.get(OSMWays.size() - 1);
+        String key = reader.getAttributeValue(null, "k");
+        String value = reader.getAttributeValue(null, "v");
+        way.addTag(key, value);
+
+    }
+
     private void addMemberToRelation(XMLStreamReader reader) {
         Relation relation = OSMRelations.get(OSMRelations.size() - 1);
         long member = Long.parseLong(reader.getAttributeValue(null, "ref"));
@@ -169,12 +188,13 @@ public class Parser {
 
     }
 
-    private void addWayToList(XMLStreamReader reader) {
+    private Way addWayToList(XMLStreamReader reader) {
         Way way = new Way();
         way.setReader(reader);
         way.setValues();
         OSMWays.add(way);
 
+        return way;
     }
 
     private void addSubElementToWay(XMLStreamReader reader) {
@@ -182,11 +202,5 @@ public class Parser {
         long id = Long.parseLong(reader.getAttributeValue(null, "ref"));
         way.addNode(nodeMap.get(id));
 
-
     }
 }
-
-
-
-
-
