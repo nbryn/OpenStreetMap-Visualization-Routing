a54e8fd3a383f50f8f443e7f8fc6f1ef59dbba87
nbryn <niklasbg@live.dk>
2020-06-22 22:52:04 +0200

    Fix errors and cleaned up tests

--- src/test/java/bfst20/logic/entities/AddressTest.java
+++ src/test/java/bfst20/logic/entities/AddressTest.java
@@ -5,48 +5,47 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AddressTest{
+public class AddressTest {
     public static Address address;
-    
+
     @BeforeAll
-    public static void setup(){
-        address = new Address("Køge","1","4600","Farstrupvej",56,54,1);
+    public static void setup() {
+        address = new Address("Køge", "1", "4600", "Farstrupvej", 56, 54, 1);
     }
 
     @Test
-    public void getstreet(){
-        assertEquals(address.getStreet(),"Farstrupvej");
+    public void getStreet() {
+        assertEquals(address.getStreet(), "Farstrupvej");
     }
 
     @Test
-    public void getHousenumber(){
-        assertEquals(address.getHouseNumber(),"1");
+    public void getHouseNumber() {
+        assertEquals(address.getHouseNumber(), "1");
     }
 
     @Test
-    public void getPostcode(){
-        assertEquals(address.getPostcode(),"4600");
+    public void getPostcode() {
+        assertEquals(address.getPostcode(), "4600");
     }
 
     @Test
-    public void getCity(){
-        assertEquals(address.getCity(),"Køge");
+    public void getCity() {
+        assertEquals(address.getCity(), "Køge");
     }
 
     @Test
-    public void getLat(){
-        assertEquals(address.getLat(),56);
+    public void getLat() {
+        assertEquals(address.getLat(), 56);
     }
 
     @Test
-    public void getLon(){
-        assertEquals(address.getLon(),54);
+    public void getLon() {
+        assertEquals(address.getLon(), 54);
     }
 
     @Test
-    public void toStringTest(){
-        assertEquals(address.toString(),"Farstrupvej 1 4600");
+    public void toStringTest() {
+        assertEquals(address.toString(), "Farstrupvej 1 4600");
     }
-
 
 }
--- src/test/java/bfst20/logic/misc/OSMTypeTest.java
+++ src/test/java/bfst20/logic/misc/OSMTypeTest.java
@@ -9,56 +9,53 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OSMTypeTest {
     public static Color color;
-    
-    
+
+
     @Test
-    public void getLineWidth(){
-        assertEquals(OSMType.getLineWidth(RESIDENTIAL_HIGHWAY,10),20);
-        assertEquals(OSMType.getLineWidth(PATH,10),10);
-    }  
-    
+    public void getLineWidth() {
+        assertEquals(OSMType.getLineWidth(RESIDENTIAL_HIGHWAY, 10), 20);
+        assertEquals(OSMType.getLineWidth(PATH, 10), 10);
+    }
+
     @Test
-    public void getZoomLevel(){
-        assertEquals(OSMType.getZoomLevel(ROUTING),1);
-        assertEquals(OSMType.getZoomLevel(HIGHWAY),1.3011416847239474E9);
-    }   
-    
+    public void getZoomLevel() {
+        assertEquals(OSMType.getZoomLevel(ROUTING), 1);
+        assertEquals(OSMType.getZoomLevel(HIGHWAY), 1.3011416847239474E9);
+    }
+
     @Test
-    public void getFill(){
-        assertEquals(OSMType.getFill(RESIDENTIAL_HIGHWAY),false);
-        assertEquals(OSMType.getFill(WOOD),true);
-    }   
-    
+    public void getFill() {
+        assertEquals(OSMType.getFill(RESIDENTIAL_HIGHWAY), false);
+        assertEquals(OSMType.getFill(WOOD), true);
+    }
+
     @Test
     public void getColorBlindColor() {
-        assertEquals(OSMType.getColor(GREEN,true), Color.LIGHTGREEN.invert());
-        assertEquals(OSMType.getColor(WATER,true), Color.rgb(170, 211, 223, 1).invert());
-        assertEquals(OSMType.getColor(MEADOW,true),Color.rgb(205, 235, 176, 1).invert());
+        assertEquals(OSMType.getColor(GREEN, true), Color.LIGHTGREEN.invert());
+        assertEquals(OSMType.getColor(WATER, true), Color.rgb(170, 211, 223, 1).invert());
+        assertEquals(OSMType.getColor(MEADOW, true), Color.rgb(205, 235, 176, 1).invert());
     }
 
     @Test
-    public void getNormalColor(){
+    public void getNormalColor() {
         color = Color.rgb(255, 241, 186, 1);
-        assertEquals(OSMType.getColor(BEACH,false),color);
-        assertEquals(OSMType.getColor(BOUNDS,false),Color.TRANSPARENT);
-        
+        assertEquals(OSMType.getColor(BEACH, false), color);
+        assertEquals(OSMType.getColor(BOUNDS, false), Color.TRANSPARENT);
     }
 
     @Test
-    public void getMaxSpeed(){
-        assertEquals(OSMType.getMaxSpeed(TERTIARY),80);
-        assertEquals(OSMType.getMaxSpeed(PATH),80);
+    public void getMaxSpeed() {
+        assertEquals(OSMType.getMaxSpeed(TERTIARY), 80);
+        assertEquals(OSMType.getMaxSpeed(PATH), 80);
     }
 
     @Test
-    public void highways(){
+    public void highways() {
         assertNotNull(OSMType.highways());
-        
     }
 
     @Test
-    public void drawables(){
+    public void drawables() {
         assertNotNull(OSMType.drawables());
     }
-    
 }
