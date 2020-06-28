789eb4605daec3d87026e3886df87ba683f54096
nbryn <niklasbg@live.dk>
2020-06-29 00:16:17 +0200

    Removed redundant code

--- src/main/java/bfst20/presentation/Launcher.java
+++ src/main/java/bfst20/presentation/Launcher.java
@@ -1,12 +1,10 @@
 package bfst20.presentation;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
 
 public class Launcher extends Application {
     public static Window primaryStage;
@@ -15,19 +13,18 @@ public class Launcher extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
 
-        //GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         FXMLLoader MainLoader = new FXMLLoader(getClass().getResource("/Main.fxml"));
-        Scene sceneMain = new Scene(MainLoader.load());//, gd.getDisplayMode().getWidth() - 100, gd.getDisplayMode().getHeight() - 100);
+        Scene sceneMain = new Scene(MainLoader.load());
 
         primaryStage.setMinHeight(720);
         primaryStage.setMinWidth(1280);
         primaryStage.setResizable(true);
+
         primaryStage.setTitle("Map");
         primaryStage.setScene(sceneMain);
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
-
 
 	public static void main(String[] args) {
         launch(args);
